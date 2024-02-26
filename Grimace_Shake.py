9081b4853763d708f6a2fad754865c439a5bf91f
Granger <julian.granger@icloud.com>
2024-02-26 14:11:13 -0500

    Added elkifs Blizzard and rainy to VRS

--- Grimace_Shake.py
+++ Grimace_Shake.py
@@ -8,8 +8,30 @@ from time import sleep
 
 #Create a function that randomly chooses the weather from a list
 def weather():
-    weatherForecast = ["Snowing", "Blizards", "Raining", "Foggy", "Windy", "Icy", "Sunny",]
+    weatherForecast = ["snowy", "blizards", "rainy", "foggy", "windy", "icy", "sunny"]
     weatherConditions = random.choice(weatherForecast)
     return weatherConditions
 
-print(weather())
+#Variable to call weather() once VRS(Vehicle Respoce System) is determined
+weatherAlert = weather()
+
+def VRS():
+    if weatherAlert == "snowy":
+        print("\nNational Weather Service has updated our alarm be 30 minutes because of the forecast of", weatherAlert,
+            "weather conditions.")
+
+        print("VRS has been enganged only allowing you to drive 50mph")
+    elif weatherAlert == "blizzard":
+        print("\nNational Weather Service has updated our alarm be 45 minutes because of the forecast of", weatherAlert,
+            "weather conditions.")
+        print("VRS has been enganged only allowing you to drive 40mph")
+    elif weatherAlert == "rainy":
+        print("\nNational Weather Service has updated our alarm be 10 minutes because of the forecast of", weatherAlert,
+            "weather conditions.")
+        print("VRS has been enganged only allowing you to drive 60mph")
+    
+
+
+
+
+VRS()

f7a42a41c4ded70845d19cdfa0e83625675d065b
Granger <julian.granger@icloud.com>
2024-02-27 13:34:34 -0500

    Finished the elif/else statements for weather - Stable

--- Grimace_Shake.py
+++ Grimace_Shake.py
@@ -8,7 +8,7 @@ from time import sleep
 
 #Create a function that randomly chooses the weather from a list
 def weather():
-    weatherForecast = ["snowy", "blizards", "rainy", "foggy", "windy", "icy", "sunny"]
+    weatherForecast = ["snowy", "blizardy", "rainy", "foggy", "windy", "icy", "sunny"]
     weatherConditions = random.choice(weatherForecast)
     return weatherConditions
 
@@ -20,7 +20,7 @@ def VRS():
         print("\nNational Weather Service has updated our alarm be 30 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
         print("VRS has been enganged only allowing you to drive 50mph")
-    elif weatherAlert == "blizzard":
+    elif weatherAlert == "blizardy":
         print("\nNational Weather Service has updated our alarm be 45 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
         print("VRS has been enganged only allowing you to drive 40mph")
@@ -28,8 +28,21 @@ def VRS():
         print("\nNational Weather Service has updated our alarm be 10 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
         print("VRS has been enganged only allowing you to drive 60mph")
-
-
+    elif weatherAlert == "foggy":
+        print("\nNational Weather Service has updated our alarm be 10 minutes because of the forecast of", weatherAlert,
+            "weather conditions.")
+        print("VRS has been enganged only allowing you to drive 50mph")
+    elif weatherAlert == "windy":
+        print("\nNational Weather Service has updated our alarm be 5 minutes because of the forecast of", weatherAlert,
+            "weather conditions.")
+        print("VRS has been enganged only allowing you to drive 70mph")
+    elif weatherAlert == "icy":
+        print("\nNational Weather Service has updated our alarm be 35 minutes because of the forecast of", weatherAlert,
+            "weather conditions.")
+        print("VRS has been enganged only allowing you to drive 45mph")
+    else:
+        print("\nNational Weather Service gives a forecast of", weatherAlert,"weather conditions.")
+        print("VRS has not been enganged")
 
 
 

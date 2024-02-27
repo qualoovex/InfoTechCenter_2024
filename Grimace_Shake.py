c28981604fb99afdd67b10e0fc4a74d555c57887
Granger <julian.granger@icloud.com>
2024-02-27 13:45:10 -0500

    called the sleep library to add between print statements - Stable

--- Grimace_Shake.py
+++ Grimace_Shake.py
@@ -19,29 +19,36 @@ def VRS():
     if weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm be 30 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
+        sleep(1)
         print("VRS has been enganged only allowing you to drive 50mph")
     elif weatherAlert == "blizardy":
         print("\nNational Weather Service has updated our alarm be 45 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
+        sleep(1)
         print("VRS has been enganged only allowing you to drive 40mph")
     elif weatherAlert == "rainy":
         print("\nNational Weather Service has updated our alarm be 10 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
+        sleep(1)
         print("VRS has been enganged only allowing you to drive 60mph")
     elif weatherAlert == "foggy":
         print("\nNational Weather Service has updated our alarm be 10 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
+        sleep(1)
         print("VRS has been enganged only allowing you to drive 50mph")
     elif weatherAlert == "windy":
         print("\nNational Weather Service has updated our alarm be 5 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
+        sleep(1)
         print("VRS has been enganged only allowing you to drive 70mph")
     elif weatherAlert == "icy":
         print("\nNational Weather Service has updated our alarm be 35 minutes because of the forecast of", weatherAlert,
             "weather conditions.")
+        sleep(1)
         print("VRS has been enganged only allowing you to drive 45mph")
     else:
         print("\nNational Weather Service gives a forecast of", weatherAlert,"weather conditions.")
+        sleep(1)
         print("VRS has not been enganged")
 
 
